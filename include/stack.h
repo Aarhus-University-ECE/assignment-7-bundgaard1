cc433478cec2b231f4a30f9dd6584c6e148bf7fa
bundgaard1 <mathiasbbjorgensen@gmail.com>
2022-10-27 17:42:09 +0200

    Header files added

--- include/stack.h
+++ include/stack.h
@@ -1,24 +1,22 @@
 #ifndef __STACK_H_
 #define __STACK_H_
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node
-{
+typedef struct node {
     int data;
     struct node* next;
 } node;
 
-
-typedef struct{
-  node* head;
+typedef struct {
+    node* head;
 } stack;
 
-void initialize(stack* );
-void push(int, stack* );
+void initialize(stack*);
+void push(int, stack*);
 int pop(stack*);
 bool empty(stack*);
 bool full(stack*);
 
-#endif // __STACK_H_
+#endif  // __STACK_H_
